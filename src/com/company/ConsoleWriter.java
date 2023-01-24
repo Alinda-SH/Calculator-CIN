06dffc512b22d3f7959a6484d83cac7c9681fe83
Alinda <70520287+Alinda-SH@users.noreply.github.com>
2023-01-24 12:44:35 +0100

    add comments to class

--- src/com/company/ConsoleWriter.java
+++ src/com/company/ConsoleWriter.java
@@ -1,6 +1,11 @@
 package com.company;
 
 public class ConsoleWriter {
+    /**
+     * Write message to the console
+     * 
+     * @param msg
+     */
     public void write(String msg) {
         System.out.println(msg);
     }
